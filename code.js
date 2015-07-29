a489fb94878aa39da5bac88ca8038f8b1598670c
Andrew <awalden0808@gmail.com>
2015-07-29 13:43:14 -0500

    Responsive!

--- code.js
+++ code.js
@@ -1,29 +1,33 @@
 var currentFeature = null; // The currently visible feature (i.e. array corresponding to jQuery object)
 
-$("#features ol li").children("a").each(function () {
-    // Allows for link behavior
-    $(this)[0].setAttribute("href", "javascript: void(0);");
+if ($("#features").length > 0) {
+    $("#features ol li").children("a").each(function () {
+        "use strict";
 
-    /*  The rollover event
-        Allows for additional classes (e.g. bottom, right, etc.) */
-    $(this).mouseenter(function () {
-        var labelClass;
-        var descriptionClass;
-        if (currentFeature !== null) {
+        // Allows for link behavior
+        $(this)[0].setAttribute("href", "javascript: void(0);");
+
+        /*  The rollover event
+            Allows for additional classes (e.g. bottom, right, etc.) */
+        $(this).mouseenter(function () {
+            var labelClass;
+            var descriptionClass;
+            if (currentFeature !== null) {
+                labelClass = currentFeature.children(".token").children(".label")[0].getAttribute("class");
+                descriptionClass = currentFeature.children(".description")[0].getAttribute("class");
+                labelClass = labelClass.replace(" visible", "");
+                descriptionClass = descriptionClass.replace(" visible", "");
+                currentFeature.children(".token").children(".label")[0].setAttribute("class", labelClass);
+                currentFeature.children(".description")[0].setAttribute("class", descriptionClass);
+            }
+            currentFeature = $(this);
             labelClass = currentFeature.children(".token").children(".label")[0].getAttribute("class");
             descriptionClass = currentFeature.children(".description")[0].getAttribute("class");
-            labelClass = labelClass.replace(" visible", "");
-            descriptionClass = descriptionClass.replace(" visible", "");
+            labelClass += " visible";
+            descriptionClass += " visible";
             currentFeature.children(".token").children(".label")[0].setAttribute("class", labelClass);
             currentFeature.children(".description")[0].setAttribute("class", descriptionClass);
-        }
-        currentFeature = $(this);
-        labelClass = currentFeature.children(".token").children(".label")[0].getAttribute("class");
-        descriptionClass = currentFeature.children(".description")[0].getAttribute("class");
-        labelClass += " visible";
-        descriptionClass += " visible";
-        currentFeature.children(".token").children(".label")[0].setAttribute("class", labelClass);
-        currentFeature.children(".description")[0].setAttribute("class", descriptionClass);
+        });
+        // TODO: Responsive!
     });
-    // TODO: Responsive!
-});
+}
