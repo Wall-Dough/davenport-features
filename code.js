cac186ad6b98914379f4b7f8f52ad8ab1a62d277
Andrew <awalden0808@gmail.com>
2015-08-04 11:11:25 -0500

    Moved token positioning to JS instead of CSS, added transitions

--- code.js
+++ code.js
@@ -1,5 +1,45 @@
 var currentFeature = null; // The currently visible feature (i.e. array corresponding to jQuery object)
 
+var images = $("#features-images img");
+
+var tokenArray = {
+    ref1 : {
+        img  : 0,
+        left : 95,
+        top  : 435
+    },
+    ref2 : {
+        img  : 0,
+        left : 180,
+        top  : 314
+    },
+    ref3 : {
+        img  : 0,
+        left : 272,
+        top  : 436
+    },
+    ref4 : {
+        img  : 0,
+        left : 389,
+        top  : 352
+    },
+    ref5 : {
+        img  : 0,
+        left : 477,
+        top  : 175
+    },
+    ref6 : {
+        img  : 0,
+        left : 691,
+        top  : 254
+    },
+    ref7 : {
+        img  : 0,
+        left : 233,
+        top  : 257
+    }
+}
+
 if ($("#features").length > 0) {
     $("#overlays").children("a").each(function () {
         "use strict";
@@ -11,6 +51,13 @@ if ($("#features").length > 0) {
         // Allows for link behavior
         $(this)[0].setAttribute("href", "javascript: void(0);");
         listRef[0].setAttribute("href", "javascript: void(0);");
+
+        var token = $(this).children(".token")[0];
+        token.style.left = (tokenArray[refId].left / images[tokenArray[refId].img].width * 100).toString() + "%";
+        token.style.top = (tokenArray[refId].top / images[tokenArray[refId].img].height * 100).toString() + "%";
+
+        $(this)[0].setAttribute("class", "img" + tokenArray[refId].img.toString());
+
 
         /*  The rollover event
             Allows for additional classes (e.g. bottom, right, etc.) */
@@ -26,6 +73,7 @@ if ($("#features").length > 0) {
                 descriptionClass = descriptionClass.replace(" visible", "");
                 currentFeature.children(".token").children(".label")[0].setAttribute("class", labelClass);
                 currentFeature.children(".description")[0].setAttribute("class", descriptionClass);
+
             }
             currentFeature = $(this);
             $("#mobileDescription")[0].innerHTML = currentFeature.children(".description")[0].innerHTML;
