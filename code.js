76da3ff79de018f9f901f57a6d1fcd39abaa4942
Andrew <awalden0808@gmail.com>
2015-08-05 14:54:06 -0500

    Added, um, a bunch of stuff.

--- code.js
+++ code.js
@@ -1,5 +1,12 @@
 var currentFeature = null; // The currently visible feature (i.e. array corresponding to jQuery object)
 
+// Breakpoint for mobile styles
+var mobileWidth = 852;
+
+// Initializes the mobile hotspot styles to be added later
+var styles = "";
+var mobileStyles = "@media only screen and (max-width: " + mobileWidth.toString() + "px) {";
+
 var images = {
     0 : {
         id: "img0",
@@ -50,42 +57,85 @@ function hideTooltip(feature) {
     if (feature === null) {
         return;
     }
-    var descriptionClass = currentFeature.children(".description")[0].getAttribute("class");
-    descriptionClass = descriptionClass.replace(" visible", "");
-    currentFeature.children(".description")[0].setAttribute("class", descriptionClass);
-    currentFeature = null;
+    $("#descriptions #" + feature.attr("id")).removeAttr("class");
 }
 
-if ($("#features").length > 0) {
+if ($("#featuresWrapper").length > 0) {
+    // Create and add the descriptions box
+    var descriptionsBox = document.createElement("section");
+    descriptionsBox.setAttribute("id", "descriptions");
+    descriptionsBox.setAttribute("style", "display: none;");
+    $("#featuresWrapper").append(descriptionsBox);
+
     $("#overlays").children("a").each(function () {
         "use strict";
 
-        var refId = $(this)[0].getAttribute("id");
+        var refId = $(this).attr("id");
 
         // Allows for link behavior
-        $(this)[0].setAttribute("href", "javascript: void(0);");
+        $(this).attr("href", "javascript: void(0);");
 
-        // Set position of hotspot
-        var hotspot = $(this).children(".hotspot")[0];
-        hotspot.style.left = (hotspotArray[refId].left / images[hotspotArray[refId].img].width * 100).toString() + "%";
-        hotspot.style.top = (hotspotArray[refId].top / images[hotspotArray[refId].img].height * 100).toString() + "%";
+        // Grab description
+        var description = $(this).children(".description")[0].innerHTML;
 
-        $(this)[0].setAttribute("class", "img" + hotspotArray[refId].img.toString());
+        // Remove description
+        $(this).children(".description").remove();
+
+        // Add description to descriptions box
+        var span = document.createElement("span");
+        span.setAttribute("id", refId);
+        span.innerHTML = description;
+        $("#descriptions").append(span);
+
+
+        // Add hotspot
+        var hotspot = document.createElement("span");
+        hotspot.setAttribute("class", "hotspot");
+        $(this).append(hotspot);
+
+        $(this).attr("class", "img" + hotspotArray[refId].img.toString());
+
+        // Adds the hotspot to the hotspot styles
+        styles += "#features #" + refId + " .hotspot {left:" + (hotspotArray[refId].left / images[hotspotArray[refId].img].width * 100).toString()
+            + "%; top:" + (hotspotArray[refId].top / images[hotspotArray[refId].img].height * 100).toString() + "%;}";
+
+        // Adds the hotspot to the mobile styles
+        mobileStyles += "#features.mobile #features-images." + refId + " {left:-" + hotspotArray[refId].left.toString() + "px;top:-" + hotspotArray[refId].top.toString() + "px;}" +
+            "#features.mobile #" + refId + " .hotspot {left:" + hotspotArray[refId].left.toString() + "px; top:" + hotspotArray[refId].top.toString() + "px;}";
 
         /*  The rollover event
             Allows for additional classes (e.g. bottom, right, etc.) */
         $(this).mouseenter(function () {
             $("#features")[0].setAttribute("class", "mobile");
-            $("#features-images")[0].setAttribute("class", $(this)[0].getAttribute("id"));
+            $("#features-images").attr("class", $(this).attr("id"));
             hideTooltip(currentFeature);
             currentFeature = $(this);
-            var descriptionClass = currentFeature.children(".description")[0].getAttribute("class");
-            descriptionClass += " visible";
-            currentFeature.children(".description")[0].setAttribute("class", descriptionClass);
+            $("#descriptions #" + $(this).attr("id")).attr("class", "visible");
         });
 
         $(this).mouseleave(function () {
             hideTooltip(currentFeature);
         });
+
+        // Adds mouseenter and mouseleave events for the descriptions
+        $("#descriptions #" + refId).mouseenter(function() {
+            $("#overlays #" + $(this).attr("id")).mouseenter();
+        });
+
+        $("#descriptions #" + refId).mouseleave(function() {
+            $("#overlays #" + $(this).attr("id")).mouseleave();
+        });
+    });
+
+    descriptionsBox.removeAttribute("style");
+
+    // Adds the mobile styles to the document head
+    mobileStyles += "}";
+    var mobileStyleElement = document.createElement("style");
+    mobileStyleElement.innerHTML = styles + mobileStyles;
+    document.head.appendChild(mobileStyleElement);
+
+    $("#expand").click(function () {
+        $("#features").removeAttr("class");
     });
 }
